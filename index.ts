d262fab82180d74374c08ee2518bc1e3b2d94730
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-10-07 10:08:35 -0300

    Add support for static files

--- index.ts
+++ index.ts
@@ -3,6 +3,9 @@ import express, { Application } from 'express';
 const app: Application = express();
 require('dotenv').config();
 app.use('/', require('./routes/RequestHandler'));
+app.use('/memes', express.static('./assets/commands/images/memes'));
+app.use('/images', express.static('./assets/commands/images'));
+
 app.listen(8080, () => {
     console.info('[FOXY API] Server is running on port 8080')
 });
