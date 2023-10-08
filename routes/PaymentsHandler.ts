90d152a4c4bdd2e374633d85142afa624bf3c8da
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-10-08 09:37:22 -0300

    Create a message per tier

--- routes/PaymentsHandler.ts
+++ routes/PaymentsHandler.ts
@@ -9,7 +9,7 @@ const router = require('express').Router();
 
 router.post('/webhook', bodyParser.raw({ type: 'application/json' }), async (req, res) => {
     const event = JSON.parse(req.body.toString());
-
+    const sig = req.headers['stripe-signature'];
     switch (event.type) {
         case 'checkout.session.completed': {
             const session = event.data.object;
@@ -51,18 +51,52 @@ router.post('/webhook', bodyParser.raw({ type: 'application/json' }), async (req
                     user.premium = true;
                     user.save();
                     try {
-                        rest.sendDirectMessage(expandedSession.custom_fields[0].text.value, {
-                            embeds: [{
-                                title: '<:foxy_yay:1070906796274888795> **|** Obrigada por assinar o premium!',
-                                description: `Obrigada por assinar o plano ${product().name}! Você recebeu **${product().cakes} Cakes!** e outros benefícios, obrigada por me ajudar a ficar online!`
-                            }]
-                        });
-                    } catch(err) { }
+                        switch (product().pType) {
+                            case 1: {
+                                rest.sendDirectMessage(expandedSession.custom_fields[0].text.value, {
+                                    title: "Obrigada por me ajudar a ficar online, yay!",
+                                    description: `Muito obrigada por comprar **${product().name}**! Você não sabe o quanto isso me ajuda (a comprar comida) e me manter online, como forma de dizer que você é uma pessoa fofa sem dizer de forma direta, você ganhou **${product().cakes.toLocaleString('pt-BR')} Cakes**! Muito obrigada! <:foxy_pray:1084184966998536303>`,
+                                    color: 0xe7385d,
+                                    footer: {
+                                        text: "Obrigada por me ajudar a ficar online!"
+                                    }
+                                });
+
+                                break;
+                            }
+
+                            case 2: {
+                                rest.sendDirectMessage(expandedSession.custom_fields[0].text.value, {
+                                    title: "Obrigada por me ajudar a ficar online, yay!",
+                                    description: `Yay! Obriada por comprar o **${product().name}**! Como forma de gratidão por me ajudar a comprar mais bolo, você ganhou **${product().cakes} Cakes. Muito obrigada seu fofo(a)!`,
+                                    color: 0xe7385d,
+                                    footer: {
+                                        text: "Obrigada por me ajudar a ficar online!"
+                                    }
+                                });
+
+                                break;
+                            }
+
+                            case 3: {
+                                rest.sendDirectMessage(expandedSession.custom_fields[0].text.value, {
+                                    title: "Obrigada por me ajudar a ficar online, yay!",
+                                    description: `Você... Comprou ${product().name}! só para me ajudar? Não tenho como descrever o quando você me ajudou com isso... Como forma de gratidão, que ainda sim não se compara a essa ajuda, você recebeu **${product().cakes} Cakes**. Muito obrigada, de verdade <:foxy_cry:1071151976504627290>`,
+                                    color: 0xe7385d,
+                                    footer: {
+                                        text: "Obrigada por me ajudar a ficar online!"
+                                    }
+                                });
+
+                                break;
+                            }
+                        }
+                    } catch (err) { }
                     break;
                 }
 
                 case 'one-time': {
-                    const user = await database.getUser(expandedSession.custom_fields[0].text.value);  
+                    const user = await database.getUser(expandedSession.custom_fields[0].text.value);
                     user.balance += product().cakes;
                     user.transactions.push({
                         type: 'bought',
@@ -72,12 +106,16 @@ router.post('/webhook', bodyParser.raw({ type: 'application/json' }), async (req
                     user.save();
                     try {
                         rest.sendDirectMessage(expandedSession.custom_fields[0].text.value, {
-                                embeds: [{
-                                    title: '<:foxy_yay:1070906796274888795> **|** Obrigada por comprar cakes!',
-                                    description: `Obrigada por comprar ${product().name}! Agora você pode utilizar na minha lojinha ou enviar para outras pessoas _ou até ficar em um rank alto rsrsrs_, obrigada por me ajudar a ficar online!`
-                                }]
+                            embeds: [{
+                                title: '<:foxy_yay:1070906796274888795> **|** Obrigada por comprar cakes!',
+                                description: `Obrigada por comprar **${product().name}**! Agora você pode utilizar na minha lojinha ou enviar para outras pessoas _ou até ficar em um rank alto rs_, obrigada por me ajudar a ficar online! <:foxy_yay:1070906796274888795>`,
+                                color: 0xe7385d,
+                                footer: {
+                                    text: "Obrigada por me ajudar a ficar online!"
+                                }
+                            }]
                         });
-                    } catch(err) { }
+                    } catch (err) { }
                     break;
                 }
             }
