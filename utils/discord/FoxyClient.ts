1c92a2bc8ea3d73694078c541f492d524090a49d
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-10-07 21:31:37 -0300

    export rest

--- utils/discord/FoxyClient.ts
+++ utils/discord/FoxyClient.ts
@@ -1,11 +1,13 @@
 import { createBot, Intents, startBot, Bot } from 'discordeno';
+import { FoxyRestManager } from '../../structures/RestManager';
 
 require('dotenv').config();
 const bot = createBot({
     token: process.env.FOXY_TOKEN,
     intents: 0 as Intents
 }) as Bot;
+const rest = new FoxyRestManager(bot);
 
 startBot(bot);
 
-export { bot };
+export { bot, rest };
