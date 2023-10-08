19552b0c5e5dc53dd19f1e413890310069d51708
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-10-07 21:31:00 -0300

    Create default message when user buy something

--- routes/PaymentsHandler.ts
+++ routes/PaymentsHandler.ts
@@ -1,5 +1,8 @@
 import bodyParser from 'body-parser';
 import { products } from '../structures/json/products.json';
+import { database } from '..';
+import { rest } from '../utils/discord/FoxyClient';
+
 const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 const router = require('express').Router();
@@ -36,12 +39,45 @@ router.post('/webhook', bodyParser.raw({ type: 'application/json' }), async (req
 
             switch (product().type) {
                 case 'subscription': {
-                    // TODO
+                    const user = await database.getUser(expandedSession.custom_fields[0].text.value);
+                    user.premiumType = product().pType;
+                    user.premiumDate = Date.now();
+                    user.transactions.push({
+                        type: 'premiumPerk',
+                        amount: product().cakes,
+                        timestamp: Date.now()
+                    });
+                    user.balance += product().cakes;
+                    user.premium = true;
+                    user.save();
+                    try {
+                        rest.sendDirectMessage(expandedSession.custom_fields[0].text.value, {
+                            embeds: [{
+                                title: '<:foxy_yay:1070906796274888795> **|** Obrigada por assinar o premium!',
+                                description: `Obrigada por assinar o plano ${product().name}! Você recebeu **${product().cakes} Cakes!** e outros benefícios, obrigada por me ajudar a ficar online!`
+                            }]
+                        });
+                    } catch(err) { }
                     break;
                 }
 
                 case 'one-time': {
-                    // TODO
+                    const user = await database.getUser(expandedSession.custom_fields[0].text.value);  
+                    user.balance += product().cakes;
+                    user.transactions.push({
+                        type: 'bought',
+                        amount: product().cakes,
+                        timestamp: Date.now()
+                    });
+                    user.save();
+                    try {
+                        rest.sendDirectMessage(expandedSession.custom_fields[0].text.value, {
+                                embeds: [{
+                                    title: '<:foxy_yay:1070906796274888795> **|** Obrigada por comprar cakes!',
+                                    description: `Obrigada por comprar ${product().name}! Agora você pode utilizar na minha lojinha ou enviar para outras pessoas _ou até ficar em um rank alto rsrsrs_, obrigada por me ajudar a ficar online!`
+                                }]
+                        });
+                    } catch(err) { }
                     break;
                 }
             }
@@ -56,8 +92,17 @@ router.post('/webhook', bodyParser.raw({ type: 'application/json' }), async (req
             });
 
             console.log(customer);
-            // TO DO: When the subscription ends, remove the premium from the user filtering by the Discord ID located in metadata
-        
+            const user = await database.getUser(customer.metadata.discord_id);
+            user.premiumType = null;
+            user.premiumDate = null;
+            user.premium = false;
+            user.save();
+            rest.sendDirectMessage(customer.metadata.discord_id, {
+                embeds: [{
+                    title: "<:foxy_cry:1071151976504627290> **|** Seu premium foi cancelado!",
+                    description: "Seu premium foi cancelado, pensei que éramos amigos <:foxy_cry:1071151976504627290>!"
+                }]
+            });
             break;
         }
     }
