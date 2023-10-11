5bdda50cbe8dc386aa911076af87769953cf0852
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-10-11 10:54:49 -0300

    Create a webhook to receive votes from top.gg

---
+++ routes/DBLHandler.ts
@@ -0,0 +1,31 @@
+import { database } from '..';
+import { rest } from '../utils/discord/FoxyClient';
+import dbl from '@top-gg/sdk';
+const router = require('express').Router();
+
+const webhook = new dbl.Webhook(process.env.DBL_TOKEN);
+
+router.post('/dbl/webhook', webhook.listener(async (vote) => {
+    const user = await database.getUser(vote.user);
+
+    if (!user) return;
+
+    try {
+        rest.sendDirectMessage(vote.user, {
+            embeds: [{
+                    "title": "Obrigada por votar!<:foxy_cake:866084383843549204>",
+                    "description": "Você sabia que votando você me ajuda a crescer? <:foxy_yay:1070906796274888795>\n\nE como recompensa você recebeu **1500 cakes**!\n\nVocê pode votar novamente em 12 horas!",
+                    color: 0xe7385d,
+                    "thumbnail": {
+                        "url": "https://cdn.discordapp.com/attachments/1078322762550083736/1161659519982637177/27_Sem_Titulo_20210215123555.png?ex=65391abc&is=6526a5bc&hm=38eafff04f0bea7a3d9f0fc154ac24875878664cf488c43ab38cf900cab48269&"
+                    },
+                    "footer": {
+                        "text": "Obrigada por votar, você é incrível!"
+                    }
+                }]
+        });
+    } catch (err) { }
+
+    user.balance += 1000;
+    await user.save();
+}));
