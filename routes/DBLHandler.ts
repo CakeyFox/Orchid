1d12a9b2529d09b00151c7d30f2e63aaccbd01e2
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2023-10-12 12:41:58 -0300

    Set a 500ms timeout to send a message

--- routes/DBLHandler.ts
+++ routes/DBLHandler.ts
@@ -10,19 +10,21 @@ router.post('/dblwebhook', webhook.listener(async (vote) => {
     if (!user) return;
 
     try {
-        rest.sendDirectMessage(vote.user, {
-            embeds: [{
-                    title: "Obrigada por votar!<:foxy_cake:866084383843549204>",
-                    description: "Você sabia que votando você me ajuda a crescer? <:foxy_yay:1070906796274888795>\n\nE como recompensa você recebeu **1500 cakes**!\n\nVocê pode votar novamente em 12 horas!",
-                    color: 0xe7385d,
-                    thumbnail: {
-                        url: "https://cdn.discordapp.com/attachments/1078322762550083736/1161659519982637177/27_Sem_Titulo_20210215123555.png?ex=65391abc&is=6526a5bc&hm=38eafff04f0bea7a3d9f0fc154ac24875878664cf488c43ab38cf900cab48269&"
-                    },
-                    footer: {
-                        "text": "Obrigada por votar, você é incrível!"
-                    }
-                }]
-        });
+        setTimeout(async () => {
+            rest.sendDirectMessage(vote.user, {
+                embeds: [{
+                        title: "Obrigada por votar!<:foxy_cake:866084383843549204>",
+                        description: "Você sabia que votando você me ajuda a crescer? <:foxy_yay:1070906796274888795>\n\nE como recompensa você recebeu **1500 cakes**!\n\nVocê pode votar novamente em 12 horas!",
+                        color: 0xe7385d,
+                        thumbnail: {
+                            url: "https://cdn.discordapp.com/attachments/1078322762550083736/1161659519982637177/27_Sem_Titulo_20210215123555.png?ex=65391abc&is=6526a5bc&hm=38eafff04f0bea7a3d9f0fc154ac24875878664cf488c43ab38cf900cab48269&"
+                        },
+                        footer: {
+                            "text": "Obrigada por votar, você é incrível!"
+                        }
+                    }]
+            });
+        }, 500);
     } catch (err) {
         console.error(err);
     }
