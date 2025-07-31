f68fdd776ff371a2aa07e155c7e4df13e637cf2f
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2025-07-30 22:32:32 -0300

    Move webhookReceiver to https://github.com/FoxyTheBot/Foxy

--- routes/DBLHandler.ts
+++ routes/DBLHandler.ts
@@ -13,57 +13,14 @@ export default class DBLHandler {
         this.router = express.Router();
         this.webhookListener = new Webhook(process.env.DBL_TOKEN);
         this.rest = new RestManager();
-        this.init();
-    }
-
-    init() {
-        this.router.post('/dblwebhook', this.webhookListener.listener(this.webhookReceiver.bind(this)));
     }
 
     getRouter() {
         return this.router;
     }
-
-    async webhookReceiver(vote) {
-        const user = await database.getUser(vote.user);
-        if (!user) return;
-
-        try {
-            setTimeout(async () => {
-                await this.rest.sendDirectMessage(vote.user, {
-                    embeds: [{
-                        title: "Obrigada por votar!<:foxy_cake:866084383843549204>",
-                        description: "Você sabia que votando você me ajuda a crescer? <:foxy_yay:1070906796274888795>\n\nE como recompensa você recebeu **1500 cakes**!\n\nVocê pode votar novamente em 12 horas!",
-                        color: 0xe7385d,
-                        thumbnail: {
-                            url: "https://cdn.discordapp.com/attachments/1078322762550083736/1161659519982637177/27_Sem_Titulo_20210215123555.png?ex=65391abc&is=6526a5bc&hm=38eafff04f0bea7a3d9f0fc154ac24875878664cf488c43ab38cf900cab48269&"
-                        },
-                        footer: {
-                            text: "Obrigada por votar, você é incrível!"
-                        }
-                    }]
-                });
-            }, 500);
-        } catch (err) {
-            console.error("Erro ao enviar mensagem de agradecimento:", err);
-        }
-
-        user.userCakes.balance += 1500;
-        user.userTransactions.push({
-            from: "Foxy",
-            to: vote.user,
-            quantity: 1500,
-            date: new Date(),
-            type: "voteReward"
-        });
-
-        user.lastVote = new Date();
-        user.voteCount = (user.voteCount || 0) + 1;
-        user.notifiedForVote = false;
-
-        await user.save();
-    }
-
+    
+    // TODO: Migrate this to Foxy's repository
+    
     async checkVoteNotifications() {
         const usersToNotify = await database.getExpiredVotes();
         usersToNotify.forEach(async (user) => {
